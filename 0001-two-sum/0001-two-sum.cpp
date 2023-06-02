4d35c7861adbdedcb33773a79e873ec10e30dd63
Mit-Darji <107803981+Mit-Darji@users.noreply.github.com>
2023-06-02 23:55:32 +0530

    Time: 8 ms (94.04%), Space: 10.8 MB (34.92%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -3,12 +3,12 @@ public:
     vector<int> twoSum(vector<int>& nums, int target) {
         unordered_map<int,int> mp;
         for(int i = 0;i < nums.size();i++){
-            int x = nums[i];
-            int y = target - x;
-            if(mp.find(y) != mp.end()){
-                return {mp[y],i};
+            int a = nums[i];
+            int b = target - a;
+            if(mp.find(b) != mp.end()){
+                return {mp[b],i};
             }
-            mp[x] = i;
+            mp[a] = i;
         }
         return {-1,-1};
     }
